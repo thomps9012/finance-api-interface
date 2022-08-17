8e56b4376be21b2d8c0c03486d7f8df95be70829
Samuel Thompson <thomps9012@gmail.com>
2022-08-17 14:45:23 -0400

    creating check request via interface

--- pages/check_request/create.tsx
+++ pages/check_request/create.tsx
@@ -2,38 +2,11 @@ import { CREATE_CHECK_REQ } from "../../graphql/mutations"
 
 import GrantSelect from "../../components/grantSelect";
 import { useMutation } from "@apollo/client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ReceiptUpload from "../../components/receiptUpload";
 import VendorInput from "../../components/vendorInput";
 import PurchaseInput from "../../components/purchaseInput";
 export default function CreateRequest() {
-    // {
-    //     "user_id": "2374fc70-737b-4bae-96e0-aeb44721e0ae",
-    //     "request": {
-    //       "date": "1203jflds",
-    //       "description": "test",
-    //       "grant_id": "test",
-    //       "receipts": ["", ""],
-    //       "credit_card": "string",
-    //       "purchases": [
-    //         {
-    //           "grant_line_item": "test1",
-    //           "description": "description",
-    //           "amount": 10.9
-    //         }
-    //       ]
-    //     },
-    //     "vendor": {
-    //       "name": "test",
-    //       "address": {
-    //         "website": "",
-    //         "street": "",
-    //         "city": "",
-    //         "state": "",
-    //         "zip": 111
-    //       }
-    //     }
-    //   }
     const [addRequest, { data, loading, error }] = useMutation(CREATE_CHECK_REQ)
     const [receipts, setReceipts] = useState([]);
     const [grantID, setGrantID] = useState("N/A")
@@ -48,6 +21,11 @@ export default function CreateRequest() {
     const [creditCard, setCreditCard] = useState("N/A")
     const [requestDate, setDate] = useState("")
     const [rowCount, setRows] = useState(1)
+    const [description, setDescription] = useState("")
+    const handleDescription = (e: any) => {
+        e.target.value.length < 76 &&
+            setDescription(e.target.value)
+    }
     const addPurchase = (e: any) => { e.preventDefault(); rowCount < 5 ? setRows(rowCount + 1) : null }
     const removePurchase = (e: any) => { e.preventDefault(); setRows(rowCount - 1) }
     if (loading) return 'Submitting...';
@@ -58,7 +36,13 @@ export default function CreateRequest() {
         if (grantID === "") { alert('select a grant'); return }
         const testUserID = '68125e1f-21c1-4f60-aab0-8efff5dc158e'
         console.log(requestDate)
-        const purchaseArr = [{ grant_line_item: "", description: "", amount: 0.0 }]
+        let purchaseArr = [];
+        const purchaseRows = document.getElementsByClassName('purchase-row');
+        for (let i = 0; i < purchaseRows.length; i++) {
+            const purchaseData = Object.fromEntries(new FormData(purchaseRows[i] as HTMLFormElement))
+            purchaseArr.push(purchaseData)
+
+        }
         addRequest({
             variables: {
                 user_id: testUserID,
@@ -71,7 +55,8 @@ export default function CreateRequest() {
                     receipts: receiptArr,
                     date: requestDate,
                     purchases: purchaseArr,
-                    credit_card: creditCard
+                    credit_card: creditCard,
+                    description: description
                 }
             }
         })
@@ -83,6 +68,9 @@ export default function CreateRequest() {
         <GrantSelect state={grantID} setState={setGrantID} />
         <h4>Date</h4>
         <input type="date" name="date" onChange={(e: any) => setDate(new Date(e.target.value).toISOString())} />
+        <h4>Description</h4>
+        <textarea rows={5} name="description" value={description} onChange={handleDescription} />
+        <span>{description.length}/75 characters</span>
         <VendorInput setAddress={setVendorAddress} setName={setVendorName} address={vendorAddress} />
         <h3>Purchases</h3>
         <span className="description">Limit of 5 Purchases per Request</span>
