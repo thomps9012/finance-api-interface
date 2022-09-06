cfe4d13a7c76066b9a155ecb9b3828df56ee98eb
Samuel Thompson <thomps9012@gmail.com>
2022-09-06 08:25:46 -0400

    added in mobile responsive navbar and page renderings

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,20 @@
-export default function Footer(){
+import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
+import styles from './header.module.css';
+export default function Footer() {
+    const { data: session } = useSession();
     return <footer>
         <h5>Copyright 2022</h5>
+        {session?.user && (
+            <div className='mobileSignOut'>
+                <a href={`/api/auth/signout`}
+                    className={styles.footerBtn} onClick={(e: any) => {
+                        e.preventDefault()
+                        signOut({ callbackUrl: '/' })
+                    }}>
+                    Sign Out
+                </a>
+            </div>
+        )}
     </footer>
 }
--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ import NavLinks from "./navLinks";
 
 export default function Header() {
     const { data: session } = useSession();
-    return <header>
-        <div className={styles.navHeader}>
+    return <>
+    <div className={styles.mobileHeader}>
             {session?.user && (
                 <>
                     <Link href="/me">
@@ -50,16 +50,65 @@ export default function Header() {
                             </Link>
                         </li>
                     </ul>
-                    <a href={`/api/auth/signout`}
-                        className={styles.buttonPrimary} onClick={(e: any) => {
-                            e.preventDefault()
-                            signOut({ callbackUrl: '/' })
-                        }}>
-                        Sign Out
-                    </a>
                 </>
             )}
         </div>
-        <NavLinks />
-    </header>
+        <header>
+            <div className={styles.navHeader}>
+                {session?.user && (
+                    <>
+                        <Link href="/me">
+                            <a>
+                                {session.user.image && (
+                                    <span
+                                        style={{ backgroundImage: `url('${session.user.image}')` }}
+                                        className={styles.avatar}
+                                    />
+                                )}
+                                <span className={styles.signedInText}>
+                                    <br />
+                                    <strong>{session.user.name}</strong>
+                                </span>
+                            </a>
+                        </Link>
+                        <ul className={styles.navIcons}>
+                            <li className={styles.navIcon}>
+                                <Link href="/">
+                                    <a className={styles.navIcon}>🏡</a>
+                                </Link>
+                            </li>
+                            <li className={styles.navIcon}>
+                                <Link href="/me/inbox">
+                                    <a className={styles.navIcon}>📭</a>
+                                </Link>
+                            </li>
+                            <li className={styles.navIcon}>
+                                <Link href="/mileage">
+                                    <a className={styles.navIcon}>🚗</a>
+                                </Link>
+                            </li>
+                            <li className={styles.navIcon}>
+                                <Link href="/petty_cash">
+                                    <a className={styles.navIcon}>💸</a>
+                                </Link>
+                            </li>
+                            <li className={styles.navIcon}>
+                                <Link href="/check_request">
+                                    <a className={styles.navIcon}>📑</a>
+                                </Link>
+                            </li>
+                        </ul>
+                        <a href={`/api/auth/signout`}
+                            className={styles.buttonPrimary} onClick={(e: any) => {
+                                e.preventDefault()
+                                signOut({ callbackUrl: '/' })
+                            }}>
+                            Sign Out
+                        </a>
+                    </>
+                )}
+            </div>
+            <NavLinks />
+        </header>
+    </>
 }
