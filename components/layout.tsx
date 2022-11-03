9ed0c0366d67b5c2bfcd42bddedae049197cd520
Samuel Thompson <thomps9012@gmail.com>
2022-11-03 15:26:03 -0400

    updated font import to head

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,19 +3,34 @@ import Footer from "./footer";
 import { useSession } from "next-auth/react";
 import AccessDenied from "./accessDenied";
 import Loading from "./loading";
-
+import Head from "next/head";
+import Link from "next/link";
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-    const { data: session, status } = useSession();
-    const loading = status === "loading";
-    if(!session && loading) {return <Loading />}
-    if (!session) { return <AccessDenied /> }
-    return <>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+  if (!session && loading) {
+    return <Loading />;
+  }
+  if (!session) {
+    return <AccessDenied />;
+  }
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </>
+  );
 }
