f1689f78489349c189274da660aca96852a410ea
Samuel Thompson <thomps9012@gmail.com>
2022-11-04 09:09:02 -0400

    added head to access denied page

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "./footer";
 import { useSession } from "next-auth/react";
 import AccessDenied from "./accessDenied";
 import Loading from "./loading";
+import Head from "next/head";
 
 interface Props {
     children: React.ReactNode
@@ -14,6 +15,14 @@ export default function Layout({ children }: Props) {
     if(!session && loading) {return <Loading />}
     if (!session) { return <AccessDenied /> }
     return <>
+     <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
         <Header />
         <main>{children}</main>
         <Footer />
