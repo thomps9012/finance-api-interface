af3a5e286b79e754bc176fc8c6aa44056b22914e
Samuel Thompson <thomps9012@gmail.com>
2022-11-07 15:18:17 -0500

    font import update

--- components/accessDenied.tsx
+++ components/accessDenied.tsx
@@ -1,27 +1,44 @@
 import { signIn } from "next-auth/react";
 import Head from "next/head";
 export default function AccessDenied() {
-    return <div style={{ margin: 100, padding: 20, display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
-         <Head>
+  return (
+    <div
+      style={{
+        margin: 100,
+        padding: 20,
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+      }}
+    >
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
         />
       </Head>
-        <h1>Access Denied</h1>
-        <div className="hr-red" />
-        <h1>You are Attempting to Visit a Protected Website</h1>
-        <br />
-        <div>
-            <a href={'/api/auth/signin'}
-                onClick={(e: any) => {
-                    e.preventDefault()
-                    signIn("google")
-                }} className="archive-btn">
-                Sign In
-            </a>
-        </div>
+      <h1>Access Denied</h1>
+      <div className="hr-red" />
+      <h1>You are Attempting to Visit a Protected Website</h1>
+      <br />
+      <div>
+        <a
+          href={"/api/auth/signin"}
+          onClick={(e: any) => {
+            e.preventDefault();
+            signIn("google");
+          }}
+          className="archive-btn"
+        >
+          Sign In
+        </a>
+      </div>
     </div>
+  );
 }
